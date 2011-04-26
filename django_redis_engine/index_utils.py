3ee38e577ac409fadaa7b7f727d55d5ffb3d4946
Mirko Rossini <mirko.rossini@ymail.com>
2011-04-26 23:52:31 +0200

    some bugfixes

--- django_redis_engine/index_utils.py
+++ django_redis_engine/index_utils.py
@@ -3,12 +3,13 @@ from django.utils.importlib import import_module
 from md5 import md5
 from redis_entity import *
 _MODULE_NAMES = getattr(settings, 'REDIS_SETTINGS_MODULES', ())
+from redis.exceptions import WatchError
 
 #TODO update might overwrite field indexes
 
 INDEX_KEY_INFIX = "redis_index"
 
-
+SWITH_INDEX_SEPARATOR = '\x02'
 
 isiterable = lambda obj: getattr(obj, '__iter__', False)
 
@@ -43,7 +44,8 @@ def create_indexes(column,data,old,indexes,conn,hash_record,table,pk,db_name):
 						if index == 'istartswith': d = d.lower()
 						if index == 'endswith': d = d[::-1]
 						if index == 'iendswith': d = d.lower()[::-1]
-						conn.zrem(get_zset_index_key(db_name,table,INDEX_KEY_INFIX,column,index),d+'_'+str(pk))
+						conn.zrem(get_zset_index_key(db_name,table,INDEX_KEY_INFIX,column,index),
+										d+SWITH_INDEX_SEPARATOR+str(pk))
 				if not isiterable(data):
 					data = (data,)
 				for d in data:
@@ -52,7 +54,8 @@ def create_indexes(column,data,old,indexes,conn,hash_record,table,pk,db_name):
 					if index == 'endswith': d = d[::-1]
 					if index == 'iendswith': d = d.lower()[::-1]
 						
-					conn.zadd(get_zset_index_key(db_name,table,INDEX_KEY_INFIX,column,index),d+'_'+str(pk),0)
+					conn.zadd(get_zset_index_key(db_name,table,INDEX_KEY_INFIX,column,index),
+										d+SWITH_INDEX_SEPARATOR+str(pk),0)
 			if index == 'exact':
 				if old is not None:
 					if not isiterable(old):
@@ -75,7 +78,8 @@ def delete_indexes(column,data,indexes,conn,hash_record,table,pk,db_name):
 					if index == 'istartswith': d = d.lower()
 					if index == 'endswith': d = d[::-1]
 					if index == 'iendswith': d = d.lower()[::-1]
-					conn.zrem(get_zset_index_key(db_name,table,INDEX_KEY_INFIX,column,index),d+'_'+str(pk))
+					conn.zrem(get_zset_index_key(db_name,table,INDEX_KEY_INFIX,column,index),
+									d+SWITH_INDEX_SEPARATOR+str(pk))
 			if index == 'exact':
 				if not isiterable(data):
 					data = (data,)
@@ -98,31 +102,37 @@ def filter_with_index(lookup,value,conn,table,column,db_name):
 		key = get_zset_index_key(db_name,table,INDEX_KEY_INFIX,column,lookup)
 
 		pipeline = conn.pipeline()		
-		pipeline.zadd(key,v,0)
+		conn.zadd(key,v,0)
 		#pipeline.zadd(key,v2,0)
-		pipeline.execute()
+		#pipeline.execute()
+		while True:
+			try:
+				conn.watch(key)
+				up = conn.zrank(key,v)
+				#down = conn.zrank(key,v2)
 
-		conn.watch(key)
-		up = conn.zrank(key,v)
-		#down = conn.zrank(key,v2)
-
-		pipeline.zrange(key,up+1,-1)#down-1)
-		pipeline.zrem(key,v)
-		#pipeline.zrem(key,v2)
-		l = pipeline.execute()
-		#print l
-		r = l[0]
-
-		#print 'erre: ',r
-		#print 'second pipeline',pipeline.execute()
-		ret = set()
-		for i in r:
-			i = unicode(i.decode('utf8'))
-			if i.startswith(v):
-				ret.add(i.split('_')[-1])
-			else:
-				break
-		return ret
+				pipeline.zrange(key,up+1,-1)#down-1)
+				pipeline.zrem(key,v)
+				#pipeline.zrem(key,v2)
+		
+				l = pipeline.execute()
+				#print l
+				r = l[0]
+				#print l
+				#print 'erre: ',r
+				#print 'second pipeline',pipeline.execute()
+				ret = set()
+				for i in r:
+					i = unicode(i.decode('utf8'))
+					if i.startswith(v):
+						splitted_string = i.split(SWITH_INDEX_SEPARATOR)
+						if len(splitted_string) > 1:
+							ret.add(splitted_string[-1])
+					else:
+						break
+				return ret
+			except WatchError:
+				pass
 #		print pipeline.execute()
 	else:
 		raise Exception('Lookup type not supported') #TODO check at index creation?
